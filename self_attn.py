d2702164fa2fae9a404b13142a468d0a646b9fe0
Han Yang <yanghanxy@users.noreply.github.com>
2019-11-05 10:45:19 +0800

    Create self_attn.py

---
+++ self_attn.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from keras import backend as K
+from keras.engine import Layer
+from keras.layers import Activation
+
+def dot_product(x, kernel):
+    if K.backend() == 'tensorflow':
+        return K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)
+    else:
+        return K.dot(x, kernel)
+
+class SelfAttention(Layer):
+    def __init__(self, **kwargs):
+        super(SelfAttention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W_q = self.add_weight((input_shape[-1], input_shape[-1]),
+                                 initializer='glorot_uniform', trainable=True,
+                                 name='{}_W_q'.format(self.name))
+        self.W_k = self.add_weight((input_shape[-1], input_shape[-1]),
+                                 initializer='glorot_uniform', trainable=True,
+                                 name='{}_W_k'.format(self.name))
+        self.W_v = self.add_weight((input_shape[-1], input_shape[-1]),
+                                 initializer='glorot_uniform', trainable=True,
+                                 name='{}_W_c'.format(self.name))
+        super(SelfAttention, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        q = dot_product(x, self.W_q)
+        k = dot_product(x, self.W_k)
+        v = dot_product(x, self.W_v)
+
+        temper = tf.sqrt(tf.cast(tf.shape(k)[-1], dtype='float32'))
+        attn = K.batch_dot(q, k, axes=[2, 2]) / temper
+        attn = Activation('softmax')(attn)
+        attn_weight = K.sum(attn, axis=1)
+        output = K.batch_dot(attn, v)
+        output = K.sum(output, axis=1)
+        return [output, attn_weight]
+
+    def compute_output_shape(self, input_shape):
+        return [(input_shape[0], input_shape[2]), input_shape[1]]
+
+    def get_config(self):
+        base_config = super(SelfAttention, self).get_config()
+        return base_config
